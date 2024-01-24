b066286388377f3873bf8b1e9e6689d3194bc47b
alisidd1 <alisiddiqui@gmail.com>
2024-01-24 14:11:41 -0500

    first commit

--- StockMarketData/ViewController/ViewController.swift
+++ StockMarketData/ViewController/ViewController.swift
@@ -4,6 +4,8 @@
 //
 //  Created by Ali Siddiqui on 1/22/24.
 //
+// REAL TIME DATA AND NEWS
+// explore FinHub API
 
 import UIKit
 
@@ -11,7 +13,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.title = "Stock Data"
+        view.backgroundColor = .systemBlue
     }
 
 
